2578477bd304e84bfa70b5517fac46ed398c8cee
Rose Gauthier <rosaleagauthier@gmail.com>
2017-04-09 16:41:35 -0400

    implement body font

--- themes/rjmconstruction/header.php
+++ themes/rjmconstruction/header.php
@@ -24,6 +24,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<?php if( is_front_page() ) : ?>
+
 			<?php $heroImage = get_field('hero'); ?>
 			<div class="site-header_hero" style="background-image: url(<?php echo $heroImage['url']; ?>);">
 
@@ -33,18 +34,23 @@
 						<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?> ">
 						<p class="tagline"><?php the_field('tagline'); ?></p>
 					</div>
+
 			</div> <!-- .hero-image -->
 
 		<?php endif; ?>
 
 		<?php if( is_page('order') ) : ?>
+
 			<?php $addressBackground = get_field('address_background', 'option'); ?>
 			<div class="address" style="background-image: url(<?php echo $addressBackground['url']; ?>);">
+
 				<div class="inner-wrapper">
 					<p class="branded-font">RJM Construction</p>
 					<p><?php the_field('address', 'option') ?></p>
 				</div>
+
 			</div>
+			
 		<?php endif; ?>
 	</header><!-- #masthead -->
 
