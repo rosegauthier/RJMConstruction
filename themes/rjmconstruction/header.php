80a18c5fb9f2b2c87504056724b74b2039167415
Rose Gauthier <rosaleagauthier@gmail.com>
2017-04-22 15:42:46 -0400

    small sass/responsive tweaks

--- themes/rjmconstruction/header.php
+++ themes/rjmconstruction/header.php
@@ -20,7 +20,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site wrapper">
+<div id="page" class="site">
 
 	<header id="masthead" class="site-header" role="banner">
 		<?php if( is_front_page() ) : ?>
